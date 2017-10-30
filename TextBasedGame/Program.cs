790bfe3e1bd789604f29100fe651fe2be6709d23
trozie <twanrozie@gmail.com>
2017-10-30 13:32:50 +0100

    inital setup

--- TextBasedGame/Program.cs
+++ TextBasedGame/Program.cs
@@ -2,11 +2,45 @@
 
 namespace TextBasedGame
 {
+    class Game
+    {
+        public static void StartGame()
+        {
+            Initialise();
+
+            Start();
+
+            End();
+        }
+
+        private static void Initialise()
+        {
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("--------SuperFancyTextGame--------");
+            Console.WriteLine("----------------------------------\n");
+        }
+
+        private static void Start()
+        {
+
+        }
+
+        private static void End()
+        {
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("----------   THE  END   ----------");
+            Console.WriteLine("----------------------------------\n");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Game.StartGame();
+
+            Console.WriteLine("Press any key to close this window");
+            Console.ReadKey();
         }
     }
 }
