1b6f7ba6c5c0ea9a419598e8954a05a1e7a555f0
trozie <twanrozie@gmail.com>
2017-11-02 13:00:02 +0100

    mini fix

--- TextBasedGame/commands/RageQuit.cs
+++ TextBasedGame/commands/RageQuit.cs
@@ -14,9 +14,7 @@ namespace TextBasedGame.commands
             Console.WriteLine("A gun magically appeares in front of you. \n" +
                               "Wanting none of this, you deside to ragequit the experiment. \n" +
                               "\"Headbutting a bullet is the fastest way out\" was the last thought that went through your head.\n");
-            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("actually the bullet was the last thing that went through your head...\n\n");
-            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(Domain.AsciiArtworks.Death);
             game.End();
         }
